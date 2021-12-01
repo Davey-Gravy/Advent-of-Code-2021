52853470a1f003df0963e3bbd879ee6eefec9114
daveygravy <daveygravy17@gmail.com>
2021-12-01 14:13:16 -0500

    completed part 2 of day 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,6 +1,8 @@
+# Part 1
 # open input file, split separate lines into a list
 file = open('day1.dat')
-data = file.read().splitlines()
+data = [int(i) for i in file.read().splitlines()]
+
 # initialize counter variable
 count = 0
 # loop through list
@@ -9,4 +11,15 @@ for i in range(len(data)-1):
     if int(data[i]) < int(data[i+1]):
         count += 1
 
+print(count)
+
+# Part 2
+# initialize counter
+count = 0
+window = 3
+for i in range(len(data)-3):
+    current_sum = sum(data[i:i+3])
+    next_sum = sum(data[i+1:i+1+3])
+    if next_sum > current_sum:
+        count += 1
 print(count)
